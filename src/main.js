fed202517f9817d6abd6001aac409ebbf18bb73f
Sebastiaan <seleddy@s.sfusd.edu>
2018-04-24 17:56:58 -0700

    did stuff
    What I did was resize game while trying to add a tile map though We didn't succeed in adding the tmx file but did go do a Json file

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(700, 392, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
 
-    game.load.image('arrow', 'assets/sprites/arrow.png');
-    game.load.image('bullet', 'assets/sprites/purple_ball.png');
+    game.load.image('arrow', 'assets/arrow.png');
+    game.load.image('bullet', 'assets/purple_ball.png');
     game.load.image('test', 'assets/test.png')
     game.load.image('border', 'assets/Window.png')
+    game.load.tilemap('test2', 'assets/test2.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('test2tiles', 'assets/test2.png');
 }
 
 var sprite;
@@ -14,12 +16,17 @@ var bullets;
 
 var fireRate = 100;
 var nextFire = 0;
-
+var map;
+var layer;
 function create() {
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.add.sprite(0,-20, 'test');
+    game.add.sprite(0,-20, 'test2tiles');
     game.add.sprite(0,0, 'border');
+    map = game.add.tilemap('test2');
+    map.addTilesetImage('Tile Layer 1', 'test2tiles');
+    layer = map.createLayer('World1');
+//    layer.resizeWorld();
     game.stage.backgroundColor = '#0000FF';
     game.stage.backgroundImage 
     bullets = game.add.group();
@@ -30,7 +37,7 @@ function create() {
     bullets.setAll('checkWorldBounds', true);
     bullets.setAll('outOfBoundsKill', true);
     
-    sprite = game.add.sprite(400, 300, 'arrow');
+    sprite = game.add.sprite(600, 350, 'arrow');
     sprite.anchor.set(0.5);
 
     game.physics.enable(sprite, Phaser.Physics.ARCADE);
