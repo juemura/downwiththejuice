939a323fe5eb3f9731102b3bca2275d3a3c87412
eileenlew89@gmail.com <missionbit@missions-MacBook-Air-4.local>
2018-05-08 16:34:57 -0700

    la
    la

---
+++ test.js
@@ -0,0 +1,7 @@
+var game = new Phaser.Game(700, 392, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+
+function preload() {
+
+    game.load.image('assets/TitleScreen.jpg');
+    <script src="TitleScreen.png"></script>
+    
