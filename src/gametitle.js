c5e50b431190d85d3530b1d998b63624fecb6df7
Sebastiaan <seleddy@s.sfusd.edu>
2018-05-08 18:40:14 -0700

    made road

--- src/gametitle.js
+++ src/gametitle.js
@@ -1,14 +1,36 @@
-var gameTitle = function(game){}
+var gameTitle = function(game){};
 
 gameTitle.prototype = {
+    
+<<<<<<< HEAD
+=======
+    
+    preload : function() {
+        this.game.load.image('gameTitle', 'assets/TitlteScreen.png');
+        this.game.load.image('TranButton', 'assets/TranButton.png');
+        this.game.load.image('TranCredit', 'assets/TranCredit.png');
+        this.game.load.image('TranHelp', 'assets/TranHelp.png');
+    },
+    
+>>>>>>> e02791b49ac674e074f3a2797f08d4cd66d659be
     create:function(){
-        var gameTitle=this.game.add.sprite(250,100, "gameTitle");
-        gameTitle.anchor.setTo(0.5, 0.5);
-        var playButton=this.game.add.button(250, 100, "play", this.playTheGame, this);
-        playButton.anchor.setTo(0.5, 0.5);
+    
+        var gameTitle=this.game.add.sprite(0,0, "gameTitle");
+//       gameTitle.anchor.setTo(0.5, 0.5);
+        var playButton=this.game.add.button(15, 316, "TranButton", this.playTheGame, this);
+        var playButton=this.game.add.button(15, 316, "TranCredit", this.playCredit, this);
+        var playButton=this.game.add.button(15, 316, "TranHelp", this.playCredit, this);
+        
+//        playButton.anchor.setTo(0.5, 0.5);
+        
+//        var btnLevel=new TextButton("Press Me",6,2,G.CHANGE_STATE,"StateLevels",32,'#ffffff');
+        //this.game.stage.backgroundImage
+//        this.bg= this.game.add.tileSprite(0, 0, this.game.stage.bounds.width, this.game.cache.getImage('gameTitle').height, 'gameTitle');
+    
+
     },
     
     playTheGame: function(){
         this.game.state.start("Main");
     }
-}
+};
